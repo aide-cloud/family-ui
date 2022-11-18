e33057865af59fbc1584193e965c6c88ce2f81ae
胡标 <biao.hu@baishan.com>
2022-11-18 13:16:10 +0800

    init npm package about family-ui:1.0.1

---
+++ family-ui-test/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text } from '@hubiao/family-ui'
+
+type Props = {}
+
+const App: React.FC = (props: Props) => {
+  return (
+    <div>
+      <Text name='App' />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+type TextProps = {
+  name: string
+}
+export const Text: FC<TextProps> = (props) => {
+  return <div>{props.name}</div>
+}
+// 重要: 模板使用的 标签名 <Text name={'xhh'} />
+Text.displayName = 'Text'
